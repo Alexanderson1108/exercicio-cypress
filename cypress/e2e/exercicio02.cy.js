dfb7c53ef4f8e495627249f1d388e8b443e335b1
Alex anderson <Alexanderson1108@hotmail.com>
2023-09-01 10:05:11 -0300

    exercicio02correçao

--- cypress/e2e/exercicio02.cy.js
+++ cypress/e2e/exercicio02.cy.js
@@ -12,15 +12,25 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
 
     beforeEach(() => {
         cy.visit('/produtos/')
+        cy.get(':nth-child(3) > .page-numbers').click({ force: true })
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        cy.compras()
+        cy.addProdutos('Cassius Sparring Tank', 'S', 'Blue')
+        cy.pagina3Produtos()
+        cy.addProdutos('Cobalt CoolTech™ Fitness Short', '33', 'Red')
+        cy.pagina3Produtos()
+        cy.addProdutos('Circe Hooded Ice Fleece', 'M', 'Green')
+        cy.pagina3Produtos()
+        cy.addProdutos('Chaz Kangeroo Hoodie', 'M', 'Gray')
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 4)
 
         //compra
 
-        cy.get('.woocommerce-message > .button').click()
-        cy.get('.checkout-button').click()
+        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+        cy.get('#order_review_heading').should('contain', 'Your order')
 
         let nomefaker = faker.name.firstName()
         let sobrenomefaker = faker.name.lastName()
@@ -48,12 +58,12 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
 
         cy.get('#terms').click()
         cy.get('#payment_method_cod').click()
-        cy.get('#place_order').click( { force:true } )
-        cy.get('.page-title').should('pedido recebido')
+        cy.get('#place_order').click({ force: true })
+        cy.get('.page-title').should('contain', 'pedido recebido')
 
         //tentei com o force mas a pagina demora a carregar mesmo assim, nao deixando eu finalizar e dando erro
 
-        
+
 
     });
 
